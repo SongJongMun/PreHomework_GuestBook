497daaa3f3fb60ab7af6a5558160c611de66719f
Laptop_SongJongMun <skuugioge@daum.net>
2016-12-30 22:41:20 +0900

    [issue][2-3] 구현 중
    JUnit Test Code 작성 - DB에 등록된 방명록 게시글 리스트 호출
    
    코드는 작동되나 테스트 결과 페이지가 404에러가 나옴, 고칠 예정(12.31)

--- guestbook/src/test/java/nhn/sample/controller/SampleControllerTest.java
+++ guestbook/src/test/java/nhn/sample/controller/SampleControllerTest.java
@@ -1,5 +1,34 @@
 package nhn.sample.controller;
 
-public class SampleControllerTest {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class SampleControllerTest {
+	private MockMvc mockMvc;
+
+	// 테스트 메소드 실행전 셋업 메소드입니다.
+	@Before
+	public void setup(){
+		// 이곳에서 HomeController를 MockMvc 객체로 만듭니다.
+		this.mockMvc = MockMvcBuilders.standaloneSetup(new SampleController()).build();
+	}
+
+	@Test
+	public void test() throws Exception{
+		// HomeController의 "/" 매핑으로 정의합니다.
+		this.mockMvc.perform(get("/guestbook/sample/openBoardList.do"))
+		// 처리 내용을 출력합니다.
+		.andDo(print())
+		// 상태값은 OK가 나와야 합니다.
+		.andExpect(status().isOk())
+		// "list"이라는 attribute가 존재해야 합니다.
+		.andExpect(model().attributeExists("list"));
+	}
 }
